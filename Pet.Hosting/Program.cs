d4fd2ad9f68c450aab36826796eef6208b342df1
speetfaiah <react94@mail.ru>
2019-10-04 14:11:15 +0300

    core 2.2

--- Pet.Hosting/Program.cs
+++ Pet.Hosting/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Pet
+namespace Pet.Hosting
 {
     public class Program
     {
--- Pet.Hosting/Startup.cs
+++ Pet.Hosting/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Pet
+namespace Pet.Hosting
 {
     public class Startup
     {
